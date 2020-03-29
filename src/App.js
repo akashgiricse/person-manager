2e25289ecb5e363dd3a2a62c159f8a26d0edb943
akashgiricse <akashgiricse@gmail.com>
2020-03-29 13:03:10 +0530

    User class based components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-const app = props => {
-  const [personState, setPersonState] = useState({
+class App extends Component {
+  state = {
     person: [
       { name: "akah", age: 34 },
       { name: "giri", age: 22 }
     ]
-  });
+  };
 
-  const [otherState, setOtherState] = useState("akashgiricse");
-
-  console.log(personState, otherState);
-
-  const switchNameHandler = () => {
-    setPersonState({
+  switchNameHandler = () => {
+    this.setState({
       person: [
         { name: "vishal", age: 22 },
         { name: "vish", age: 58 }
       ]
     });
   };
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to react app</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.person[0].name}
+          age={this.state.person[0].age}
+        />
+        <Person
+          name={this.state.person[1].name}
+          age={this.state.person[1].age}
+        />
+      </div>
+    );
+  }
+}
 
-  return (
-    <div className="App">
-      <h1>Welcome to react app</h1>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person
-        name={personState.person[0].name}
-        age={personState.person[0].age}
-      />
-      <Person
-        name={personState.person[1].name}
-        age={personState.person[1].age}
-      />
-    </div>
-  );
-};
-
-export default app;
+export default App;
